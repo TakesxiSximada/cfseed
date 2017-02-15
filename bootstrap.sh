1c09950338af6a3da8d9cecc9a4b222920149a4d
TakesxiSximada <takesxi.sximada@gmail.com>
2017-02-15 23:36:16 +0900

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,5 @@
 curl -L https://github.com/TakesxiSximada/cfseed/archive/master.tar.gz | gzip -dc | tar -xv
 mv cfseed-master .cfseed
+cp .cfseed/Makefile.tmpl Makefile
+cp -R .cfseed/environ .
+cp -R .cfseed/templates .
